e18ac34824ad6d5bbadce784f3e1b81d52541c31
Timothy McCurrach <tim.mccurrach@gmail.com>
2020-05-28 23:32:54 +0100

    Update FormTemplate

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -3,14 +3,19 @@ import React from "react";
 import { Form } from "react-final-form";
 import { arrayMutators } from "react-final-form-named-arrays";
 
-const FormTemplate = (props) => (
-	<Form
-		onSubmit={props.onSubmit || (() => {})}
-		mutators={arrayMutators}
-		render={({ handleSubmit }) => (
-			<form onSubmit={handleSubmit}>{props.children}</form>
-		)}
-	></Form>
-);
+const FormTemplate = (props) => {
+	const { onSubmit, ...otherProps } = props;
+
+	return (
+		<Form
+			onSubmit={props.onSubmit || (() => {})}
+			mutators={arrayMutators}
+			render={({ handleSubmit }) => (
+				<form onSubmit={handleSubmit}>{props.children}</form>
+			)}
+			{...otherProps}
+		></Form>
+	);
+};
 
 export default FormTemplate;
