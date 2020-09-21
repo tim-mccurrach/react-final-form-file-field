0baee7efc2e2a002a11193b37df1572f1633f385
Timothy McCurrach <tim.mccurrach@gmail.com>
2020-09-21 14:16:33 +0100

    Update dist

--- dist/FileItem.js
+++ dist/FileItem.js
@@ -5,45 +5,38 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.FileItem = FileItem;
+exports.FileItem = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+var _objectWithoutProperties2 = _interopRequireDefault(require("@babel/runtime/helpers/objectWithoutProperties"));
 
 var _react = _interopRequireDefault(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _airbnbPropTypes = require("airbnb-prop-types");
-
 var _reactFontawesome = require("@fortawesome/react-fontawesome");
 
 var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
-
-var _reactBeautifulDnd = require("react-beautiful-dnd");
 
 var _mimeTypeMaps = require("./mimeTypeMaps");
 
 var _styledComponents = require("./styledComponents");
 
-/*
-export default function DraggableFileItem(props) {
-	return (
-		<Draggable
-			key={props.fileName}
-			index={props.index}
-			draggableId={props.fileName}
-		>
-			{(provided) => <FileItem dragProps={provided} {...props} />}
-		</Draggable>
-	);
-}
-*/
-function FileItem(props) {
+var FileItem = /*#__PURE__*/_react["default"].forwardRef(function (props, ref) {
+  var fileName = props.fileName,
+      removeFile = props.removeFile,
+      downloadFile = props.downloadFile,
+      inProgress = props.inProgress,
+      progress = props.progress,
+      mimeType = props.mimeType,
+      dragProps = props.dragProps,
+      otherProps = (0, _objectWithoutProperties2["default"])(props, ["fileName", "removeFile", "downloadFile", "inProgress", "progress", "mimeType", "dragProps"]);
   var fileType = (0, _mimeTypeMaps.getFileType)(props.mimeType);
   var icon = _mimeTypeMaps.fileTypeToFaIcon[fileType];
   return /*#__PURE__*/_react["default"].createElement(_styledComponents.FileItemLi, (0, _extends2["default"])({
-    ref: props.dragProps.innerRef
-  }, props.dragProps.draggableProps, props.dragProps.dragHandleProps), /*#__PURE__*/_react["default"].createElement(_styledComponents.StyledFaIcon, {
+    ref: ref
+  }, otherProps), /*#__PURE__*/_react["default"].createElement(_styledComponents.StyledFaIcon, {
     icon: icon,
     iconColor: fileType
   }), !props.inProgress && typeof props.downloadFile === "string" && /*#__PURE__*/_react["default"].createElement(_styledComponents.DownloadButton, {
@@ -67,19 +60,16 @@ function FileItem(props) {
     value: props.progress,
     max: 100
   }));
-}
+});
 
-FileItem.defaultProps = {
-  dragProps: {}
-};
-FileItem.propTypes = (0, _airbnbPropTypes.forbidExtraProps)({
+exports.FileItem = FileItem;
+FileItem.propTypes = {
   // required
   fileName: _propTypes["default"].string.isRequired,
-  progress: _propTypes["default"].number.isRequired,
   removeFile: _propTypes["default"].func.isRequired,
   //optional
   downloadFile: _propTypes["default"].oneOfType([_propTypes["default"].func, _propTypes["default"].string]),
   inProgress: _propTypes["default"].bool,
-  mimeType: _propTypes["default"].string,
-  dragProps: _propTypes["default"].object
-});
+  progress: _propTypes["default"].number,
+  mimeType: _propTypes["default"].string
+};
