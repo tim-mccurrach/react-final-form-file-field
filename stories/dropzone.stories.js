097d05d2feaa6c67ce521ce454b776224cf4b286
Timothy McCurrach <tim.mccurrach@gmail.com>
2020-09-21 14:10:45 +0100

    update stories to remove use of dragProps

--- stories/dropzone.stories.js
+++ stories/dropzone.stories.js
@@ -17,7 +17,14 @@ const DraggableFileItem = (props) => (
 		index={props.index}
 		draggableId={props.fileName}
 	>
-		{(provided) => <FileItem dragProps={provided} {...props} />}
+		{(provided) => (
+			<FileItem
+				ref={provided.innerRef}
+				{...provided.draggableProps}
+				{...provided.dragHandleProps}
+				{...props}
+			/>
+		)}
 	</Draggable>
 );
 
