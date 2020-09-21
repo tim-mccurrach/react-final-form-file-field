15f7617a0bb9593003a4c836d086de07378ccc00
Timothy McCurrach <tim.mccurrach@gmail.com>
2020-09-21 13:55:25 +0100

    Remove tests from dist

--- .babelrc.js
+++ .babelrc.js
@@ -4,4 +4,5 @@ const test = NODE_ENV === "test";
 module.exports = {
 	presets: ["@babel/preset-env", "@babel/preset-react"],
 	plugins: ["@babel/plugin-transform-runtime"],
+	ignore: ["**/*.test.js"],
 };
--- dist/utils.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _utils = require("./utils");
-
-describe("addVersionToFilename", function () {
-  test.each([["foo.bar", 2, "foo(2).bar"], ["foo.bar.bar", 3, "foo.bar(3).bar"]])("(%s, %i) => %s", function (filename, version, expected) {
-    expect((0, _utils.addVersionToFilename)(filename, version)).toEqual(expected);
-  });
-});
