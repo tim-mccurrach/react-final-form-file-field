7d14e20a2a8be58cef26eaf1d26585c91bbfff82
Timothy McCurrach <tim.mccurrach@gmail.com>
2020-09-22 00:27:01 +0100

    Bump patch version and rebuild dist

--- dist/utils.js
+++ dist/utils.js
@@ -3,12 +3,39 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.addVersionToFilename = void 0;
+exports.addVersionToFilename = exports.countDuplicates = void 0;
+
+var countDuplicates = function countDuplicates(array, value) {
+  var name = value.substring(0, value.lastIndexOf("."));
+  var extension = value.split(".").pop();
+  var pattern;
+
+  if (name) {
+    pattern = "".concat(name, "(\\(\\d+\\))?\\.").concat(extension);
+  } else {
+    pattern = "".concat(extension, "(\\(\\d+\\))?");
+  }
+
+  var count = 0;
+  array.forEach(function (item) {
+    if (item.match(pattern)) {
+      count += 1;
+    }
+  });
+  return count;
+};
+
+exports.countDuplicates = countDuplicates;
 
 var addVersionToFilename = function addVersionToFilename(filename, version) {
   var name = filename.substring(0, filename.lastIndexOf("."));
   var extension = filename.split(".").pop();
-  return "".concat(name, "(").concat(version, ").").concat(extension);
+
+  if (name) {
+    return "".concat(name, "(").concat(version, ").").concat(extension);
+  }
+
+  return "".concat(extension, "(").concat(version, ")");
 };
 
 exports.addVersionToFilename = addVersionToFilename;
